fd66aa115a6a6dcbea7f51623ff4f14c72ebda6e
Kaity G. B <uberChicGeekChick@uberChicGeekChick.Com>
2008-09-30 01:25:39 -0600

    Added ini_set( "date.timezone", "America/Denver" ); to fix a strict error that was making the exported OPML unreadalbe by most clients.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	//To access Alacast's OPML add this QUERY_STRING '?subscribe=export&format=OPML' to Alacast URI.
 	ini_set("display_errors", "true");
 	ini_set("error_reporting", E_ALL | E_STRICT);
+	ini_set( "date.timezone", "America/Denver" );
 
 	require_once("functions/support.php");
 	require_once("functions/display.php");
