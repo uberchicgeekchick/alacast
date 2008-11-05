0d4feaa288dc49c38f08f151cb72861933a87904
Kaity G. B <uberChick@uberChicGeekChick.Com>
2008-11-05 15:35:02 -0700

    I added an additional mirror for 'News Real w/Sean Kennedy'.  I kinda like this show but the host is a loon though less looney than a lot of people.  But I do like some of the news that he covers.  I am also continuing work on adding support for m3u playlists.

--- podcast_arrays/03-Culture-03.podcasts.php
+++ podcast_arrays/03-Culture-03.podcasts.php
@@ -1,50 +1,26 @@
-<?PHP
-	/* ATTN:  Come on hon, work on: Alacast, gPodder, something more!
-	 * Please, don't get distracted working on these files!
-	 * Be proud to be an uberChick; Open Source Artist; a DevChick!
-	 *
-	 * So we all can get our communites &podcasts started growing - so I can find my place
-	 * Be proud &happy of being a welcome part of the community to which we belong;  something larger than myself.
-	 *
-	 * I'm above other things I'm a:
-	 * 	program, code, devevlop, engineer software.
-	 * Know it!  Feel it! Be it! Be proud of it!
-	 */
+<?php
 	return array(
-		'Volcanicast: Google Zeitgeist'=>array(
-			'www'=>"http://www.volcanicast.com/",
+		'Learn Out Loud&#39;s Lit Summary Podcast' => array(
+			'www' => "http://www.learnoutloud.com/Catalog/Literature/American-Classics/Lit-Summary-Podcast/24192",
 			'rss'=>array(
-				'default'=>"http://planetretcon.com/podcast/volcanicast.xml",
+				'default'=>"http://feeds.feedburner.com/LitSummaryPodcast",
 			),
 			'tags'=>"",
 		),
-		'Driving the Digital Revolution'=>array(
-			'www'=>"http://www.cis.cornell.edu/alumniblog/",
+		'NewsReal w/Sean Kennedy' => array(
+			'www' => "",
 			'rss'=>array(
-				'default'=>"http://www.cis.cornell.edu/alumniblog/feed/",
+				'default'=>"http://bur.st/~clintman/cgi-bin/newsreal.cgi/podcast.xml",
+				'Mirror #1'=>"http://umap.binrev.com/?id=rantradio_newsreal&podcast=1",
 			),
 			'tags'=>"",
 		),
-		'Digital Tipping Point'=>array(
-			'www'=>"http://www.archive.org/details.php?identifier=digitaltippingpoint",
-			'rss'=>array(
-				'default'=>"http://www.archive.org/services/collection-rss.php?collection=digitaltippingpoint",
-			),
-			'tags'=>"",
-		),
-		''=>array(
-			'www'=>"",
+		'' => array(
+			'www' => "",
 			'rss'=>array(
 				'default'=>"",
 			),
 			'tags'=>"",
 		),
-		''=>array(
-			'www'=>"",
-			'rss'=>array(
-				'default'=>"",
-			),
-			'tags'=>"",
-		)
 	);
 ?>
--- podcast_arrays/useful.regexp.tcsh
+++ podcast_arrays/useful.regexp.tcsh
@@ -20,4 +20,3 @@
 #	Coverting OPMLs to PHP Array:
 #		s/^.*title=\('[^']\+'\).*xmlUrl='\([^']\+\)'.*htmlUrl='\([^']\+\)'.*/\t\t\1=>array(\r\t\t\t'www'=>"\3",\r\t\t\t'rss'=>array(\r\t\t\t\t"\2",\r\t\t\t),\r\t\t),/
 #
-
