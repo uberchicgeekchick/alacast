10319d382887ec53f3a041fe3760a00d71334df0
Kaity G. B <uberChick@uberChicGeekChick.Com>
2008-11-15 01:40:16 -0700

    I finished converting my php arrays to OPMLs.  I still have to merge the separate podcasts & remove any duplicates.  I also need to implement OPML support to all of Alacast.  I will not implement this fully until I am implementing my new version of Alacast.  Which I will write using clutter or XULRunner, most likely XULRunner.

--- OPMLs/01-opsound.org: open  source music-01.podcasts.php
+++
@@ -1,2 +0,0 @@
-		<outline title='Opsound: genre algorithmic' xmlUrl='http://opsound.org/feed/genre/algorithmic' type='rss' text='Opsound: genre algorithmic' htmlUrl='http://opsound.org/genre/algorithmic' description=''/>
-		<outline title='Opsound: genre ambient' xmlUrl='http://opsound.org/feed/genre/ambient' type='rss' text='Opsound: genre ambient' htmlUrl='http://opsound.org/genre/ambient' description=''/>
