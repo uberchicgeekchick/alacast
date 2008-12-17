72978249d66e35cadd6b16240d45fca7610b1921
Kaity G. B <uberChick@uberChicGeekChick.Com>
2008-12-17 06:51:11 -0700

    I am just adding some networking features to Alacast v2 and organizing the file structure Alacast eventually merging with AOPHP.

---
+++ v2/Network/Downloader.class.php
@@ -0,0 +1,62 @@
+<?php
+	/*
+	 * (c) 2007-Present Kathryn G. Bohmont <uberChicGeekChick.Com -at- uberChicGeekChick.Com>
+	 * 	http://uberChicGeekChick.Com/
+	 * Writen by an uberChick, other uberChicks please meet me & others @:
+	 * 	http://uberChicks.Net/
+	 *I'm also disabled; living with Generalized Dystonia.
+	 * Specifically: DYT1+/Early-Onset Generalized Dystonia.
+	 * 	http://Dystonia-DREAMS.Org/
+	 */
+
+	/*
+	 * Unless explicitly acquired and licensed from Licensor under another
+	 * license, the contents of this file are subject to the Reciprocal Public
+	 * License ("RPL") Version 1.5, or subsequent versions as allowed by the RPL,
+	 * and You may not copy or use this file in either source code or executable
+	 * form, except in compliance with the terms and conditions of the RPL.
+	 *
+	 * All software distributed under the RPL is provided strictly on an "AS
+	 * IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND
+	 * LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
+	 * LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+	 * PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the RPL for specific
+	 * language governing rights and limitations under the RPL.
+	 *
+	 * ---------------------------------------------------------------------------------
+	 * |	A copy of the RPL 1.5 may be found with this project or online at	|
+	 * |		http://opensource.org/licenses/rpl1.5.txt					|
+	 * ---------------------------------------------------------------------------------
+	 */
+	
+	/*
+	 * TODO
+	 *	Have fun with this project!
+	 *	If not pick another or do something different.
+	 *	Find joy!
+	 */
+	namespace Alacast::Network;
+
+	class Downloader extends Alacast::Network{
+		public $Public_Variale;
+		private $Private_Variable;
+		static $Static_Variable;
+
+
+		public function __construct(){
+		}//__constuct
+
+		private function Clear_Numbers(&$Titles){
+			$Total_Titles=count($Titles);
+			for($i=0; $i<$Total_Titles; $i++)
+				$Titles[$i]=preg_replace("/^([^0-9])([0-9])/", "0$1$2", (
+					preg_replace("/\([^0-9]\)\([0-9]\)\([^0-9]\)/", "\10\2\3/", (
+						preg_replace( "/\([^0-9]\)\([0-9]\)$/", "\10\2/", $Titles[$i] )
+					)
+				);
+		}//Clear_Numbers
+
+		public function __destruct(){
+		}//__destruct
+	}
+?>
