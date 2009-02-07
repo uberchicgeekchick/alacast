1612546366bc795a764bafa0855a341c1732f7de
uberChick <uberChick@uberChicGeekChick.Com>
2009-02-07 05:35:20 -0700

    Created using large amounts of pain induced unsomnia.

--- bin/scripts/alacast:catalogs:search.pl
+++ bin/scripts/alacast:catalogs:search.pl
@@ -29,13 +29,13 @@ sub search_catalog{
 		$opml_and_outline =~ s/[\r\n]+//g;
 		my $opml_file = $opml_and_outline;
 		#$opml_file =~ s/^$opml_files_path\///;
-		$opml_file =~ s/^(.*):\t.*$/\1/;
+		$opml_file =~ s/^(.*):[0-9]+:\t.*$/\1/;
 
 		my $opml_attribute = $opml_and_outline;
 		$opml_attribute =~ s/.*$output=["']([^"']+)["'].*/\1/;
 		$opml_attribute =~ s/<!\[CDATA\[(.+)\]\]>/\1/;
 
-		printf( "%s@%s\n", $opml_attribute, $opml_file );
+		printf( "%s @ %s\n", $opml_attribute, $opml_file );
 		
 		if($be_verbose){printf($opml_and_outline);}
 	}
